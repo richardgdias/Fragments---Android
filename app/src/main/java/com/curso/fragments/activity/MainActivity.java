92c0bbe94041927508de11f929419ab283397c0f
Richard Dias <richard_gdias@hotmail.com>
2021-03-09 13:28:36 -0300

    Fragments

---
+++ app/src/main/java/com/curso/fragments/activity/MainActivity.java
@@ -0,0 +1,61 @@
+package com.curso.fragments.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.curso.fragments.R;
+import com.curso.fragments.fragments.ContatosFragment;
+import com.curso.fragments.fragments.ConversasFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonConversa, buttonContato;
+    private ConversasFragment conversasFragment;
+    private ContatosFragment contatoFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonContato = findViewById(R.id.buttonContato);
+        buttonConversa = findViewById(R.id.buttonConversa);
+
+        //remover sombra da ActionBar
+        getSupportActionBar().setElevation(0);
+
+        conversasFragment = new ConversasFragment();
+
+        //configurar objeto para o fragmento
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameConteudo, conversasFragment);
+        transaction.commit();
+
+        buttonContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contatoFragment = new ContatosFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, contatoFragment);
+                transaction.commit();
+            }
+        });
+
+        buttonConversa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conversasFragment = new ConversasFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, conversasFragment);
+                transaction.commit();
+            }
+        });
+
+    }
+}
